6625d8d1781172a8808c0723ca3acd6a37091644
amitdec26 <40913399+amitdec26@users.noreply.github.com>
2018-07-28 16:42:10 +0530

    mgmr rejection trial

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>DevOps PHP mgmr project July 28th 2018, code need mgmr approval</h1>
+<h1>DevOps PHP mgmr project July 28th 2018, code need mgmr rejection trial</h1>
 
 <?php
 echo "version 2.0";
